e54c8003d6770469ebbc5007b08122132abd449e
Zsolt Toth <tothzs87@gmail.com>
2022-03-21 10:50:47 +0100

    feat(rest-api): add swagger and document for car router

--- rest-api/app.js
+++ rest-api/app.js
@@ -15,6 +15,37 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'MERN Template Express API with Swagger',
+            version: '0.1.0',
+            description:
+                'Lorem ipsum ...',
+            license: {
+                name: 'MIT',
+                url: 'https://spdx.org/licenses/MIT.html'
+            },
+            contact: {
+                name: 'Zsolt Toth',
+                url: 'https://github.com/ZsoltToth',
+                email: 'toth.zsolt@uni-eszterhazy.hu'
+            }
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000/'
+            }
+        ]
+    },
+    apis: [path.join(__dirname, '/routes/*.js')]
+};
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, { explorer: true }));
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/cars', carsRouter);
--- rest-api/routes/cars.js
+++ rest-api/routes/cars.js
@@ -2,8 +2,40 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/carController')
 
+/**
+ * @swagger
+ * /api/cars:
+ *  get:
+ *      summary: GET all cars
+ *      parameters:
+ *          -   name: color
+ *              in: query
+ *              type: string
+ *              required: false
+ *          -   name: brand
+ *              in: query
+ *              type: string
+ *              required: false
+ *      responses:
+ *          200:
+ *              description: OK
+ */
 router.get('/', controller.fetchCars)
 
+/**
+ * @swagger
+ * /api/cars/{id}:
+ *  get:
+ *      summary: get a single car by plate number
+ *      parameters:
+ *          -   name: id
+ *              in: path
+ *              type: string
+ *              required: true
+ *      responses:
+ *          200:
+ *              description: OK
+ */
 router.get('/:id', controller.fetchCarById)
 
 module.exports = router;
